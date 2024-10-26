24b5a5454ea3103b548a81dc25f84d3b5aaecc7b
Ahmad Ghanavati <Ahmad_ghanavati@yahoo.com>
2024-10-26 11:19:57 +0330

    init version 0.7 , 0.8 fix lint errors change meta name and configKey change some codes in plugin for use moment library

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -4,9 +4,7 @@ import 'tippy.js/dist/tippy.css' // optional for styling
 
 export default defineNuxtPlugin((nuxtApp) => {
   console.log('Plugin injected by Cap Jalali Calendar Nuxt3!')
-
   nuxtApp.vueApp.use(moment)
-
   // nuxtApp.provide('momentJalali', moment)
   nuxtApp.vueApp.use(VueTippy)
 })
